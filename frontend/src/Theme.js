ff0618f24d6cf6a11a200e2f8d0940d3ee2121a0
ishaa-m <isha09091@gmail.com>
2023-10-03 12:22:12 +0800

    added theme for frontend

---
+++ frontend/src/Theme.js
@@ -0,0 +1,24 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#1C2B36',
+        },
+        secondary: {
+            main: '#343A40',
+        },
+        button: {
+            main: '#0D4EA6',
+        },
+    },
+    typography: {
+        fontFamily: 'Inter',
+    },
+});
+export default function App() {
+    return (
+        <ThemeProvider theme={theme}>
+        </ThemeProvider>
+    );
+}
