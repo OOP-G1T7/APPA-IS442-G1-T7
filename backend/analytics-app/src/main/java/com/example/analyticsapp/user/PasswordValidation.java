a91d1d43fc7ef57c009b57bd4a04b4b5a3044bc6
Musyaffaq <Musyaffaq@users.noreply.github.com>
2023-10-06 00:32:12 +0800

    Add Register feature

---
+++ backend/analytics-app/src/main/java/com/example/analyticsapp/user/PasswordValidation.java
@@ -0,0 +1,65 @@
+package com.example.analyticsapp.user;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidation {
+    public static void validatePassword(String password) {
+        ArrayList<String> errors = new ArrayList<String>();
+        if (password.length() < 8) {
+            errors.add("Password too short");
+        }
+        if (password.length() > 25) {
+            errors.add("Password too long");
+        }
+        if (!containsUpperCase(password)) {
+            errors.add("Password requires an upper case");
+        }
+        if (!containsLowerCase(password)) {
+            errors.add("Password requires a lower case");
+        }
+        if (!containsDigit(password)) {
+            errors.add("Password requires a number");
+        }
+        if (!containsSymbol(password)) {
+            errors.add("Password requires a symbol");
+        }
+        if (errors.size() != 0) {
+            throw new InvalidPasswordException(errors.toString());
+        }
+    }
+
+    public static boolean containsUpperCase(String input) {
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                return true; // Found an uppercase character
+            }
+        }
+        return false; // No uppercase characters found
+    }
+
+    public static boolean containsLowerCase(String input) {
+        for (char c : input.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                return true; // Found a lower character
+            }
+        }
+        return false; // No lowercase characters found
+    }
+
+    public static boolean containsDigit(String input) {
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return true; // Found a digit
+            }
+        }
+        return false; // No digits found
+    }
+
+    public static boolean containsSymbol(String input) {
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find(); // Returns true if at least one symbol is found
+    }
+}
--- backend/analytics-app/src/main/java/com/example/analyticsapp/user/UserRepository.java
+++ backend/analytics-app/src/main/java/com/example/analyticsapp/user/UserRepository.java
@@ -1,9 +1,6 @@
 package com.example.analyticsapp.user;
 
-import java.util.ArrayList;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,22 +8,4 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-
-    /**
-     * Find all users.
-     *
-     * @return The user if found, or null if not found.
-     */
-    @Query(value = "SELECT * FROM user", nativeQuery = true)
-    ArrayList<UserEntity> getAllUsers();
-
-    /**
-     * Find all user by their username.
-     *
-     * @param username The username to search for.
-     * @return The user if found, or null if not found.
-     */
-    @Query(value = "SELECT * FROM user where username = ?", nativeQuery = true)
-    UserEntity getOneUser(String username);
-
 }
