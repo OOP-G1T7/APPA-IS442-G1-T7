25db94d8dd3aad5da956e26917963c813b958256
leannchong <leann_chong@hotmail.com>
2023-10-22 22:19:30 +0800

    Added StockSearch endpoint
    Added error handling for retrieving stock data

--- backend/analytics-app/src/main/java/com/example/analyticsapp/stockwrapper/StockWrapperController.java
+++ backend/analytics-app/src/main/java/com/example/analyticsapp/stockwrapper/StockWrapperController.java
@@ -1,67 +1,56 @@
 package com.example.analyticsapp.stockwrapper;
 
-import java.time.LocalDate;
-import java.util.*;
-
-import org.json.JSONObject;
+import com.example.analyticsapp.stockwrapper.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/stockwrapper")
 public class StockWrapperController {
+
     @GetMapping("/{stockTicker}")
-    public ResponseEntity<List<Map<String, String>>> getStock(@PathVariable String stockTicker) {
-        String apiKey = "demo"; // Replace with your API key
+    public ResponseEntity<?> getStock(@PathVariable String stockTicker) {
+        String apiKey = "4U3NNSG5OHR1CBIG";
 
-        StockWrapperService stockService = new StockWrapperService(apiKey);
-        JSONObject stockData = stockService.getStockData(stockTicker);
-
-        if (stockData != null) {
-            List<Map<String, String>> result = new ArrayList<>();
-
-            // Extract the relevant time series data based on the specified timeframe
-            JSONObject timeSeries = stockData.getJSONObject("Time Series (Daily)");
-
-            // Create a TreeMap to automatically sort the data by date
-            TreeMap<LocalDate, String> sortedData = new TreeMap<>();
-
-            for (String dateStr : timeSeries.keySet()) {
-                JSONObject timeData = timeSeries.getJSONObject(dateStr);
-                String closingPrice = timeData.getString("4. close");
-
-                // Parse the date string into a LocalDate object
-                LocalDate date = LocalDate.parse(dateStr);
-
-                sortedData.put(date, closingPrice);
-            }
-
-            // Convert the sorted data into the desired format
-            for (Map.Entry<LocalDate, String> entry : sortedData.entrySet()) {
-                Map<String, String> dataPoint = new HashMap<>();
-
-                // Format the date as a string (if needed)
-                String formattedDate = entry.getKey().toString();
-
-                dataPoint.put(formattedDate, entry.getValue());
-                result.add(dataPoint);
-            }
+        try {
+            StockWrapperService stockService = new StockWrapperService(apiKey);
+            List<Map<String, String>> result = stockService.getDailyStockData(stockTicker);
 
             return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(result);
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Collections.emptyList());
+        } catch (TickerNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
+    @GetMapping("/stockSearch/{search}")
+    public ResponseEntity<?> searchStocks(@PathVariable String search) {
+        String apiKey = "4U3NNSG5OHR1CBIG";
+
+        try {
+            StockWrapperService stockService = new StockWrapperService(apiKey);
+            List<Map<String, String>> result = stockService.searchStocks(search);
+
+            return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
-
-
