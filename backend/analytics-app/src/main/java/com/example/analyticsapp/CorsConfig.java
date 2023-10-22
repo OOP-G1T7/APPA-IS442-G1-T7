e4b6db3e984a90950def35aa50442435c50012e4
sheena <39979188+sheenaljy@users.noreply.github.com>
2023-10-22 20:32:28 +0800

    sign in & sign up linked to backend

---
+++ backend/analytics-app/src/main/java/com/example/analyticsapp/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.example.analyticsapp;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
