fc0d4016497fe95a443f5b977e40babaea20315e
Eugene Yan <artyzheng58@gmail.com>
2023-10-23 17:00:05 +0800

    Reset password module WIP
    Need to propagate changes of password to db

---
+++ backend/analytics-app/src/main/java/com/example/analyticsapp/resetpassword/ResetPasswordController.java
@@ -0,0 +1,57 @@
+package com.example.analyticsapp.resetpassword;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.analyticsapp.user.UserEntity;
+import com.example.analyticsapp.user.UserRepository;
+import com.example.analyticsapp.user.UserService;
+
+@RestController
+@RequestMapping("/password-reset")
+public class ResetPasswordController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private ResetPasswordService resetService;
+
+
+    // Not sure how we want to give the token, so function output not set yet
+    @PostMapping("/send-token")
+    public String forgotPassword(@RequestParam String email) {
+        UserEntity user = userRepo.getUserByEmail(email);
+
+        if (user.equals(null)) {
+            return "No such user";
+        }
+
+        ResetPasswordToken token = resetService.createToken(user);
+        String tokenString = token.getToken();
+        return tokenString;
+    }
+
+    @PostMapping("/resetting")
+    public UserEntity resetPassword(@RequestParam String token, @RequestParam String password) {
+        ResetPasswordToken tokenStored = resetService.findByToken(token);
+
+        // check if the token generated exists in the database and whether it expired
+        if (tokenStored.equals(null) || tokenStored.hasExpired()) {
+            return null; 
+        }
+
+        UserEntity user = tokenStored.getUser();
+        user.setPassword(password);
+        return user;
+    }
+
+}
---
+++ backend/analytics-app/src/main/java/com/example/analyticsapp/resetpassword/ResetPasswordToken.java
@@ -0,0 +1,94 @@
+package com.example.analyticsapp.resetpassword;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.example.analyticsapp.user.UserEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "password_reset_token")
+public class ResetPasswordToken {
+    
+    private static final int EXPIRATION = 60 * 24; // 24 hours
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "token")
+    private String token;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private UserEntity user;
+
+    @Column(name = "expiry_date")
+    private Date expiryDate;
+
+    public ResetPasswordToken() {
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    public ResetPasswordToken(String token, UserEntity user) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = calculateExpiryDate(EXPIRATION);
+    }
+
+    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean hasExpired() {
+        return new Date().after(this.expiryDate);
+    }
+}
+
