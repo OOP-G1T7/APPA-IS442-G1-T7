08b14071fbfc92ce1e2b1d1042a38024020ae5a7
ishaa-m <isha09091@gmail.com>
2023-10-31 17:21:52 +0800

    portfolio creation page

--- frontend/src/pages/PortfolioCreation.js
+++ frontend/src/pages/PortfolioCreation.js
@@ -19,17 +19,25 @@ import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import TableBody from '@mui/material/TableBody';
 import Navbar from '../components/Navbar';
+import axios from "axios";
 
 
 export default function Portfolio() {
     const [selectedEquities, setEquities] = useState([]);
+    const [allEquities, setAllEquities] = useState([]);
+    const [loadedEquities, setLoadedEquities] = useState([]);
+    const [selectedTickers, setSelectedTickers] = useState([]);
+    const [portfolioName, setPortfolioName] = useState('');
+    const [portfolioDescription, setPortfolioDescription] = useState('');
+    const [portfolioCapital, setPortfolioCapital] = useState('');
 
-    const equities = [
-        { symbol: "TSCDF", name: "Tesco", type: "Equity", region: "United States" },
-        { symbol: "APPL", name: "Apple", type: "Equity", region: "United States" },
-        { symbol: "AMAZON", name: "Amazon", type: "Equity", region: "Frankfurt" },
-        { symbol: "TESLA", name: "TESLA", type: "Equity", region: "Frankfurt" },
-    ];
+    // Initialize the total variable
+    let totalProportion = 0;
+
+    // Map through the selected equities and calculate the total proportion
+
+
+    // Now 'totalProportion' contains the sum of all proportions
 
     const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
     const checkedIcon = <CheckBoxIcon fontSize="small" />;
@@ -38,9 +46,80 @@ export default function Portfolio() {
         setEquities(value);
     };
 
+    const handleAutocompleteSearch = (event, value) => {
+        // Implement search logic
+        const filteredOptions = allEquities.filter(option => (
+            option.symbol.toLowerCase().includes(value.toLowerCase()) ||
+            option.name.toLowerCase().includes(value.toLowerCase())
+        ));
+
+        // Update loadedEquities state with the new options
+        setLoadedEquities(filteredOptions.slice(0, 10));
+    };
+
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        const bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpc2hhMDkwOTFAZ21haWwuY29tIiwiZXhwIjoxOTEzNzc3Mzg2fQ.PzTcrkdvWVr4DlYwDC0jOS5D--Jm1vVyL7q6dK6dO-I";
+
+        const headers = {
+            Authorization: `Bearer ${bearerToken}`
+        };
+
+        axios.post(`http://localhost:8080/api/portfolio`, {
+            userId: 1,
+            name: portfolioName,
+            description: portfolioDescription,
+            capital: portfolioCapital
+        }, { headers })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+
+
     };
+
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpc2hhMDkwOTFAZ21haWwuY29tIiwiZXhwIjoxOTE0NzQxMzAyfQ.ON26Ln93QLNjY2xQBJaqXSNCIOEaVR8URuBorxcpujk';
+
+    const getData = async () => {
+        try {
+            const res = await axios.get("/api/stockwrapper/getStockListing", {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            console.log(res.data)
+            setAllEquities(res.data);
+            setLoadedEquities(res.data.slice(0, 10));
+        } catch (error) {
+            console.log("API unavailable")
+        }
+    };
+
+    const handleTickers = (tickerObj) => {
+        const updatedArray = selectedTickers.map(item => {
+            if (item.ticker === tickerObj.ticker) {
+                return { ...item, proportion: tickerObj.proportion };
+            }
+            return item;
+        });
+
+        const existingObject = updatedArray.find(item => item.ticker === tickerObj.ticker);
+        if (!existingObject) {
+            updatedArray.push({ ticker: tickerObj.ticker, proportion: tickerObj.proportion });
+        }
+
+        setSelectedTickers(updatedArray);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+
 
     return (
         <>
@@ -59,20 +138,21 @@ export default function Portfolio() {
                             <Grid item minHeight={100} container spacing={2} justifyContent="space-between">
                                 <Grid item xs={6}>
                                     <Typography>Portfolio Name</Typography>
-                                    <TextField id="outlined-basic" defaultValue="Portfolio 1" variant="outlined" style={{ width: "80%" }} />
+                                    <TextField id="outlined-basic" variant="outlined" style={{ width: "80%" }} onChange={(event) => setPortfolioName(event.target.value)} />
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Typography>Capital</Typography>
-                                    <TextField id="outlined-basic" defaultValue={0} variant="outlined" style={{ width: "80%" }} type="number" />
+                                    <TextField id="outlined-basic" variant="outlined" style={{ width: "80%" }} type="number" onChange={(event) => setPortfolioCapital(event.target.value)} />
                                 </Grid>
                                 <Grid item xs={6}>
                                     <Typography>Add stocks to portfolio</Typography>
                                     <Autocomplete
+                                        onInputChange={(event, value) => handleAutocompleteSearch(event, value)}
                                         onChange={(event, value) => handleChange(event, value)}
                                         multiple
                                         popupIcon={<SearchIcon />}
                                         id="checkboxes-tags-demo"
-                                        options={equities}
+                                        options={loadedEquities}
                                         disableCloseOnSelect
                                         sx={{
                                             width: 300,
@@ -104,6 +184,7 @@ export default function Portfolio() {
                                         rows={4}
                                         maxRows={Infinity}
                                         style={{ width: "80%" }}
+                                        onChange={(event) => setPortfolioDescription(event.target.value)}
                                     />
                                 </Grid>
 
@@ -122,22 +203,10 @@ export default function Portfolio() {
                                                             <TableRow key={equity.name}>
                                                                 <TableCell><b>{equity.name}</b></TableCell>
                                                                 <TableCell><TextField type="number"
-                                                                    label="Quantity"
-                                                                    InputProps={{ inputProps: { min: 1 } }}
+                                                                    label="Proportion"
+                                                                    id={equity.symbol}
                                                                     onChange={(event) =>
-                                                                        event.target.value < 1
-                                                                            ? (event.target.value = 1)
-                                                                            : event.target.value
-                                                                    }
-                                                                ></TextField></TableCell>
-                                                                <TableCell><TextField
-                                                                    label="Capital"
-                                                                    type="number"
-                                                                    InputProps={{ inputProps: { min: 1 } }}
-                                                                    onChange={(event) =>
-                                                                        event.target.value < 1
-                                                                            ? (event.target.value = 1)
-                                                                            : event.target.value
+                                                                        handleTickers({ ticker: event.target.id, proportion: event.target.value })
                                                                     }
                                                                 ></TextField></TableCell>
                                                             </TableRow>
