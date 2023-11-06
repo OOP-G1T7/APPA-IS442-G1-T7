d69ffb76f65ae4b90df7b3959cd54edd1cb075e3
sheena <39979188+sheenaljy@users.noreply.github.com>
2023-11-06 09:07:58 +0800

    Updated Account js UI

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -2,137 +2,99 @@ import Navbar from "../components/Navbar";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Button from '@mui/material/Button';
-import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 
+const Container = styled("div")(({ theme }) => ({
+  background: `linear-gradient(to bottom, ${
+    theme.palette.mode === "dark" ? "#1A2027" : "#fff"
+  }, ${
+    theme.palette.mode === "dark" ? "#111418" : "#f2f2f2"
+  })`,
+  paddingBottom: theme.spacing(8), // Adjust the vertical spacing here
+}));
+
+const Content = styled("div")(({ theme }) => ({
+  padding: theme.spacing(2),
+  maxWidth: 400,
+  margin: "0 auto",
+  backgroundColor: theme.palette.background.paper,
+  borderRadius: theme.shape.borderRadius,
+  boxShadow: theme.shadows[3],
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  textAlign: "center",
+  marginTop:"60px"
+}));
+
+const AvatarContainer = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  gap: "20px", // Adjust the gap as needed
+  marginBottom:"40px",
+  marginTop:"20px"
+});
+
 export default function Account() {
-  
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-
   return (
-    <div>
-      
+    <Container>
       <Navbar />
-
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={3}
-      >
-        <Grid item xs={12}>
-          <div
-            style={{
-              position: "relative",
-              display: "block",
-              top: "27px",
-              backgroundColor: "#c9392e",
-              width: "100%",
-              height: "150px",
+      <Content>
+        <AvatarContainer>
+          <Avatar
+            alt="Profile Pic"
+            src="profile-pic.jpg"
+            sx={{
+              width: "100px",
+              height: "100px",
             }}
-          >
-            <Grid
-              container
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              spacing={3}
-            >
-              <Grid item xs={12}>
-                <div
-                  style={{
-                    position: "relative",
-                    display: "block",
-                    top: "27px",
-                    backgroundColor: "#c9392e",
-                    width: "100%",
-                    height: "150px",
-                  }}
-                >
-                  <h2
-                    style={{
-                      color: "white",
-                      position: "absolute",
-                      left: "6rem",
-                      bottom: "10px",
-                    }}
-                  >
-                    Account Details
-                  </h2>
-
-                  <Avatar
-                    alt="Profile Pic"
-                    src="profile-pic.jpg"
-                    sx={{
-                      width: "100px",
-                      height: "100px",
-                      position: "absolute",
-                      right: "-2rem",
-                      margin: "30px 0px 30px 0",
-                    }}
-                  />
-                </div>
-              </Grid>
-            </Grid>
-          </div>
-        </Grid>
-      </Grid>
-
-      <Box sx={{ flexGrow: 1, margin: "100px 100px 20px 100px" }}>
+          />
+          <h2>Account Details</h2>
+        </AvatarContainer>
         <Grid container spacing={2}>
-          <Grid item xs={12} md={12}>
+          <Grid item xs={12} md={6}>
             <TextField
               label="First Name"
               defaultValue="Jennifer"
               InputProps={{
                 readOnly: true,
               }}
-              sx={{ width: "100%", marginBottom: "20px" }}
+              sx={{ width: "100%", marginBottom: 2 }}
             />
+          </Grid>
+          <Grid item xs={12} md={6}>
             <TextField
               label="Last Name"
               defaultValue="Williams"
               InputProps={{
                 readOnly: true,
               }}
-              sx={{ width: "100%" }}
+              sx={{ width: "100%", marginBottom: 2 }}
             />
           </Grid>
-
-          <Grid item xs={6} md={12}>
+          <Grid item xs={12}>
             <TextField
               label="Email"
               defaultValue="jenniferwilliams89@gmail.com"
               InputProps={{
                 readOnly: true,
               }}
-              sx={{ width: "100%", marginBottom: "20px" }}
+              sx={{ width: "100%", marginBottom: 2 }}
             />
           </Grid>
-
-          <Grid item xs={6} md={12}>
+          <Grid item xs={12}>
             <Button
-                href="/ChangePassword"
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Change Password
-              </Button>
+              href="/ChangePassword"
+              variant="contained"
+              sx={{ width: "100%" }}
+            >
+              Change Password
+            </Button>
           </Grid>
         </Grid>
-      </Box>
-    </div>
+      </Content>
+    </Container>
   );
 }
