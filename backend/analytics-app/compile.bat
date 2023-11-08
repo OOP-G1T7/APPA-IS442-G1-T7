ad2e5f52f2f92d4def7b7e9699943aaf908f66e4
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-11-09 00:55:52 +0800

    Add powershell option to install mvn

--- backend/analytics-app/compile.bat
+++ backend/analytics-app/compile.bat
@@ -7,10 +7,20 @@ set "MAVEN_VERSION=4.0.0"
 :: Check if Maven is installed
 where mvn >nul 2>&1
 if %errorlevel% neq 0 (
-    :: If Maven is not installed, download and install it
-    echo Maven is not installed. Installing...
-    bitsadmin.exe /transfer "Maven" "https://archive.apache.org/dist/maven/maven-3/%MAVEN_VERSION%/binaries/apache-maven-%MAVEN_VERSION%-bin.zip" "%TEMP%\apache-maven-%MAVEN_VERSION%-bin.zip"
+    :: If Maven is not installed, attempt to download and install it using wget
+    echo Maven is not installed. Attempting to download with wget...
+    wget "https://archive.apache.org/dist/maven/maven-3/%MAVEN_VERSION%/binaries/apache-maven-%MAVEN_VERSION%-bin.zip" -O "%TEMP%\apache-maven-%MAVEN_VERSION%-bin.zip"
+    
+    :: Check the exit code of wget to determine success or failure
+    if %errorlevel% neq 0 (
+        :: If wget fails, download Maven using PowerShell
+        echo wget failed. Downloading with PowerShell...
+        powershell -Command "Invoke-WebRequest -Uri 'https://archive.apache.org/dist/maven/maven-3/%MAVEN_VERSION%/binaries/apache-maven-%MAVEN_VERSION%-bin.zip' -OutFile '%TEMP%\apache-maven-%MAVEN_VERSION%-bin.zip'"
+    )
+    
+    :: Extract the downloaded Maven distribution
     tar.exe -xf "%TEMP%\apache-maven-%MAVEN_VERSION%-bin.zip" -C "%TEMP%"
+    
     set "MAVEN_HOME=%TEMP%\apache-maven-%MAVEN_VERSION%"
     set "PATH=%MAVEN_HOME%\bin;%PATH%"
 )
